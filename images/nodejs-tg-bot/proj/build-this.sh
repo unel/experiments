fd11166b84c9e8ccb0c0c96ff18d31a4e9c4de0c
unel <unel86.dev+co@proton.me>
2023-05-10 16:25:33 +0000

    update image version in build script

--- images/nodejs-tg-bot/proj/build-this.sh
+++ images/nodejs-tg-bot/proj/build-this.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it -v `pwd`/source/:/workdir/source -v `pwd`/destination:/workdir/destination npm-builder:0.0.5
+docker run --rm -it -v `pwd`/source/:/workdir/source -v `pwd`/destination:/workdir/destination npm-builder:0.0.7
