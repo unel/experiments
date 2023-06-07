42638879f3f12ccf1ed0ee323da1df33a2e4e479
unel <unel86.dev+co@proton.me>
2023-06-07 13:48:24 +0000

    fibery/us#9 simplify style

--- apps/webfront/src/lib/components/Listener.svelte
+++ apps/webfront/src/lib/components/Listener.svelte
@@ -22,6 +22,10 @@
 		sr.stopListening();
 	}
 
+	function toggleListening() {
+		(isListening ? stopListening : startListening)();
+	}
+
 	function onStatusUpdate(status) {
 		isListening = status.isActive;
 
@@ -51,9 +55,9 @@
 
 </script>
 
-<section>
-	<button class="btn" on:click={startListening} disabled={isListening}> listen me </button>
-	<button class="btn" on:click={stopListening} disabled={!isListening}> stop this </button>
-
-	<span class="State">[{isListening ? 'listen' : 'deaf'}]</span>
-</section>
+<button class='btn btn-mr2' on:click={toggleListening} aria-selected={isListening}>
+	{#if isListening}
+	<svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path d="M8 4a4 4 0 1 1 0 8 4 4 0 0 1 0-8Z"></path></svg>
+	{/if}
+	<span>{isListening ? 'listening..' : 'listen'}</span>
+</button>
