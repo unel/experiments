43d6a63aaf5d567f47a4466defa630aa12c405a3
unel <unel86.dev+github@proton.me>
2023-06-15 15:56:22 +0400

    fibery/us#9 extend client api for loading and creating chat messages

--- apps/webfront/src/lib/api.ts
+++ apps/webfront/src/lib/api.ts
@@ -1,25 +1,120 @@
-import { build } from "vite";
-
-const BASE_URL = 'https://experiments.unl'
+const BASE_URL = 'https://local.experiments.unl';
+const BASE_API_URL = '/api/chat/v1';
 
 type FetchFN = typeof fetch;
 type FetchArgs = Parameters<FetchFN>;
 
-function buildUrl(path: string, query = {}): string {
+type ScalarRecord = Record<string, number | string | boolean>;
+
+type ServerChat = {
+    id: string,
+    userId: string,
+    createdAt: string,
+    title: string,
+    messages?: Array<ChatMessage>,
+};
+
+type ServerChatMessage = {
+    id: string,
+    chatId: string,
+    userId: string,
+    createdAt: string,
+    language: string,
+    text: string,
+};
+
+export type Chat = ServerChat & {
+    createdAt: Date;
+}
+
+export type ChatMessage = ServerChatMessage & {
+    createdAt: Date;
+}
+
+function buildUrl(methodPath: string, query = {}): string {
     const searchParams = new URLSearchParams(query);
-    return `${BASE_URL}${path}${searchParams.toString()}`;
+    return `${BASE_URL}${BASE_API_URL}${methodPath}?${searchParams.toString()}`;
 }
 
 async function fetchJSON(fetchFn: FetchFN, ...args: FetchArgs) {
-    const response = await fetchFn(...args);
+    try {
+        const response = await fetchFn(...args);
+        const json = await response.json();
+        return json;
 
-    return response.json();
+    } catch (e) {
+        console.error('failed to fetch', e);
+        throw ({
+            error: e
+        })
+    }
+
+}
+
+const defaultHeaders = {
+    'Content-Type': 'application/json',
+};
+
+function mapChat(chat: ServerChat): Chat {
+    if (chat.createdAt) {
+        chat.createdAt = new Date(chat.createdAt);
+    }
+
+    if (chat.messages) {
+        chat.messages = chat.messages.map(message => mapChatMessage(message));
+    }
+
+    return chat;
+}
+
+function mapChatMessage(message: ServerChatMessage): ChatMessage {
+    if (message.createdAt) {
+        message.createdAt = new Date(message.createdAt);
+    }
+
+    return message;
 }
 
 const api = {
-    loadChats: async (fetchFn: typeof fetch, params: Record<string, string> = {}) => {
-        return fetchJSON(fetchFn, buildUrl('/api/chat/v1/chats', params));
+    loadChats: async (fetchFn: FetchFN, params: ScalarRecord = {}): Promise<Array<Chat>> => {
+        const sChats = await fetchJSON(fetchFn, buildUrl('/chats', params));
+
+        return sChats.map(chat => mapChat(chat));
     },
+
+    createChat: async (fetchFn: FetchFN, { userId, title }): Promise<Chat> => {
+        const sChat = await fetchJSON(fetchFn, buildUrl('/chats'), {
+            method: 'POST',
+            headers: defaultHeaders,
+            body: JSON.stringify({
+                title,
+                userId,
+            }),
+        });
+
+        return mapChat(sChat);
+    },
+
+    createMessage: async (fetchFn: FetchFN, { userId, language, chatId, text }): Promise<ChatMessage> => {
+        const sMessage = await fetchJSON(fetchFn, buildUrl(`/chats/${chatId}/messages`), {
+            method: 'POST',
+            headers: defaultHeaders,
+            body: JSON.stringify({
+                userId,
+                language,
+                text,
+            })
+        });
+
+        return mapChatMessage(sMessage);
+    },
+
+    removeMessage: async (fetchFn: FetchFN, { chatId, messageId }): Promise<{ ok: boolean }> => {
+        return fetchJSON(fetchFn, buildUrl(`/chats/${chatId}/messages/${messageId}`), {
+            method: 'DELETE',
+            headers: defaultHeaders,
+        });
+    }
 }
 
 
