798cae03717355cdc6c53dddf098ef49aa623ab2
unel <unel86.dev+github@proton.me>
2023-06-15 15:54:54 +0400

    fibery/us#9 add withMessages for get chats api

--- apps/chatapi/src/routes/v1/chats/+server.ts
+++ apps/chatapi/src/routes/v1/chats/+server.ts
@@ -2,11 +2,30 @@ import { json } from '@sveltejs/kit';
 
 import { db } from '$lib/db';
 
+function safeJSONParse(strValue: string | undefined | null): number | string | boolean | undefined | null {
+    if (typeof strValue === 'string') {
+        try {
+            return JSON.parse(strValue);
+        } catch (_) {
+            // just pass
+        }
+    }
+
+    return strValue;
+}
+
 export async function GET({ url }) {
     const userId = url.searchParams.get('userId');
+    const withMessages = Boolean(safeJSONParse(url.searchParams.get('withMessages')));
     const query = userId ? { userId } : {}
     const chats = await db.chat.findMany({
         where: query,
+        orderBy: {
+            createdAt: 'desc',
+        },
+        include: {
+            messages: withMessages,
+        }
     });
 
     return json(chats);
