546d40fd52aa0376e815eb4c41ce31c48b1a1f70
unel <unel86.dev+github@proton.me>
2023-06-22 20:22:26 +0400

    fibery/us#23 add "universal crud" api for entities

---
+++ apps/gptapi/src/routes/v1/crud/[className]/+server.ts
@@ -0,0 +1,31 @@
+import { json } from '@sveltejs/kit';
+
+import { db } from '$lib/db';
+import { getTableName } from './utils';
+
+// get list of entitites
+export async function GET({ params }) {
+    const tableName = getTableName(params);
+    if (!tableName || !db[tableName]) {
+        console.log('misskey', params, tableName, Object.keys(db));
+        return new Response();
+    }
+
+    const result = await db[tableName].findMany();
+
+    return json(result);
+}
+
+// create new entity
+export async function POST({ request, params }) {
+    const tableName = getTableName(params);
+    if (!tableName || !db[tableName]) {
+        console.log('misskey', params, tableName);
+        return new Response();
+    }
+
+    const payload = await request.json();
+    const result = await db[tableName].create({ data: payload });
+
+    return json(result);
+}
