f379a3a9717a25d423732e598c43a5b68ef14b06
unel <unel86.dev+github@proton.me>
2023-06-23 11:43:15 +0400

    little refactoring seed file

--- apps/gptapi/prisma/seed.js
+++ apps/gptapi/prisma/seed.js
@@ -1,23 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-
-import { MODELS_LIST } from '../src/lib/data/gpt-models-list.js';
-import { TEMPLATES_LIST } from '../src/lib/data/prompt-templates.js';
-import {
-	THREAD_TEMPLATES_LIST,
-	PROMPT_TEMPLATES_LIST,
-	THREAD_WAYS_LIST,
-} from '../src/lib/data/thread-templates.js';
+import { DATA } from './seed-data.js';
 
 const prisma = new PrismaClient();
-
 async function main() {
-	const DATA = [
-		['GptModel', MODELS_LIST],
-		['PromptTemplate', TEMPLATES_LIST.concat(PROMPT_TEMPLATES_LIST)],
-		['ThreadTemplate', THREAD_TEMPLATES_LIST],
-		['ThreadTemplateWay', THREAD_WAYS_LIST],
-	];
-
 	await Promise.all(
 		DATA.flatMap(([className, items]) => items.map(data => ensureData(className, data))),
 	);
